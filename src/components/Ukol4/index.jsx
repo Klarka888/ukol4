0f1de265cb7331f58b1dd29086d05df726f3cdc1
Klarka888 <91918804+Klarka888@users.noreply.github.com>
2024-02-28 11:30:57 +0100

    Add files via upload

---
+++ src/components/Ukol4/index.jsx
@@ -0,0 +1,61 @@
+import { useState } from 'react';
+
+/*
+Zadání 1: Vytvořte stavy pro login a heslo a propojte je obousměrně s příslušnými inputy
+  ve formuláři.
+Zadání 2: Na formulář pověstě obsluhu události `onSubmit`. Nezapomeňte na `e.preventDefault`.
+  Po odeslání formuláře vypište do konzole objekt se dvěma vlastnostmi: `login` a `password`,
+  které budou obsahovat aktuální hodnoty z inputů.
+Zadání 3: Vytvořte si stav `error`, bude obsahovat zprávu pro uživatele, pokud něco vyplní špatně.
+  Na začátku bude mít prázdnou hodnotu prázdný řetězec. Zobrazte obsah tohoto stavu v prvku
+  `.error`.
+Zadání 4: Pokud uživatel nechal prázdné pole s loginem, nastavte při odeslání formuláře do stavu
+  `error` text `Chybí login`.
+Zadání 5: Pokud uživatel nechal prázdné pole s heslem, nastavte při odeslání formuláře do stavu
+  `error` text `Chybí heslo`.
+Zadání 6: Pokud uživatel správně vyplnil obě pole, do stavu `error` nastavte text `Přihlášení
+  proběhlo úspěšně`.
+*/
+
+export const Ukol4 = () => {
+
+  const [login, setLogin] = useState('');
+  const [heslo, setHeslo] = useState('');
+  const [error, setError] = useState('');
+
+  const kontrolaLogin = (l) => {
+    l === ''? setError('Chybi Login') : setError('Login ok')
+  }
+
+
+  return (
+    <>
+
+<div className="error" >{kontrolaLogin(l)}</div>
+
+     
+
+
+      <form onSubmit={(e) => {
+      e.preventDefault;
+     // setLogin(login);
+     // setHeslo(heslo);
+      console.log(login);
+      console.log(heslo)}}>
+        <div className="field">
+          <label>
+            Login:
+            <input type="text" onChange={(e) => setLogin(e.target.value)} />
+          </label>
+        </div>
+        <div className="field">
+          <label>
+            Heslo:
+            <input type="password" onChange={(e) => setHeslo(e.target.value)}/>
+          </label>
+        </div>
+        <button type="submit">Přihlásit</button>
+      </form>
+    </>
+  );
+};
